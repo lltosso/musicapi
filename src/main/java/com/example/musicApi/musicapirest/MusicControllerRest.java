1cef1fd1a274b84866470a86f2b7d878a4218501
Luis Tosso <telerisa305@gmail.com>
2025-03-22 17:04:23 +0100

    MusicControllerRest added

--- src/main/java/com/example/musicApi/musicapirest/MusicControllerRest.java
+++ src/main/java/com/example/musicApi/musicapirest/MusicControllerRest.java
@@ -1,5 +1,25 @@
 package com.example.musicApi.musicapirest;
 
+@RestController
+@RequestMapping("api")
 public class MusicControllerRest {
+List<Music> musicList;
 
-}
+    @PostConstruct
+    public void loadData() {
+        musicList = new ArrayList<>();
+
+        musicList.add(new Music( "Manager));
+        musicList.add(new Music( "Artista"));
+
+    }
+
+    @GetMapping("/rock")
+    public List<Student> listStudents() {
+        return studentList;
+    }
+
+    @GetMapping("/clasica")
+    public String hola() {
+        return musicList;
+    }
