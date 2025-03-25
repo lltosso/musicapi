a433d54b2827b5da98aa4d33d643946f734ecdb2
nawud <duwan17@gmail.com>
2025-03-25 11:51:54 +0100

    musicApi completed

--- src/main/java/com/example/musicApi/security/SecurityConfig.java
+++ src/main/java/com/example/musicApi/security/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
